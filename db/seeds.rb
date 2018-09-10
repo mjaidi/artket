069eb0098518c7d43f96c453489086753af3d985
Majid Jaidi <majid.jaidi@gmail.com>
2018-09-10 14:38:01 +0100

    fixed seeds and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,8 @@ a_millenia_ago = now - 365*200
 puts "Adding Data"
 
 #seeding users
-test_user = User.create!(first_name: "Majid", last_name: "Jaidi", email: "majid@gmail.com", password: "123456789")
+user1 = User.create!(first_name: "Majid", last_name: "Jaidi", email: "majid@gmail.com", password: "123456789")
+user2 = User.create!(first_name: "Yasmina", last_name: "Hannaoui", email: "yas@gmail.com", password: "123456789")
 
 #seeding categories
 categories.each_with_index do |category, index|
@@ -67,10 +68,10 @@ start_categories_id = Category.first.id
 end_categories_id = Category.last.id
 
 #seeding Galleries
-gallery1 = Gallery.create!(user_id: test_user.id, name: "Artket", description: Faker::Lorem.paragraph(2, false, 6), address_line: "Jemaa El Fna", city: "Marrakesh",country: "Morocco", email: "artket@artket.ma", phone: "0522625545")
+gallery1 = Gallery.create!(user_id: user2.id, name: "Artket", description: Faker::Lorem.paragraph(2, false, 6), address_line: "Jemaa El Fna", city: "Marrakesh",country: "Morocco", email: "artket@artket.ma", phone: "0522625545")
 gallery1.remote_photo_url = gallery_photos[0]
 gallery1.save
-gallery2 = Gallery.create!(user_id: test_user.id, name: "Thema", description: Faker::Lorem.paragraph(2, false, 6),  address_line: "Maarif", city: "Casablanca",country: "Morocco", email: "thema@thema.ma", phone: "0524512548" )
+gallery2 = Gallery.create!(user_id: user1.id, name: "Thema", description: Faker::Lorem.paragraph(2, false, 6),  address_line: "Maarif", city: "Casablanca",country: "Morocco", email: "thema@thema.ma", phone: "0524512548" )
 gallery2.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536584377/thema-casablanca-maroc.jpg"
 gallery2.save
 
@@ -83,47 +84,47 @@ exhibition2.remote_cover_photo_url = exhibition_photos[1]
 exhibition2.save
 
 #seeding artists
-  artist1 = Artist.create!(first_name: "Amparo", last_name: "Cruz Herrera", description: "Fille du célèbre peintre José Cruz Herrera Linense", birth_date: Date.new(1926,10,4), death_date: Date.new(2003,11,12))
+  artist1 = Artist.create!(first_name: "Amparo", last_name: "Cruz Herrera", description: "Fille du célèbre peintre José Cruz Herrera Linense", birth_date: Date.new(1926,10,4), death_date: Date.new(2003,11,12), user_id: user1.id)
   artist1.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536570709/amparo_cruz_herrera.jpg"
   artist1.save
 #seeding artists
-  artist2 = Artist.create!(first_name: "Hassan", last_name: "El Glaoui", description: "Peintre marocain formé à l'Académie des Beaux Arts de Paris", birth_date: Date.new(1924,9,4), death_date: Date.new(2003,11,12))
+  artist2 = Artist.create!(first_name: "Hassan", last_name: "El Glaoui", description: "Peintre marocain formé à l'Académie des Beaux Arts de Paris", birth_date: Date.new(1924,9,4), death_date: Date.new(2003,11,12), user_id: user1.id)
   artist2.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536571162/5f0765_e61c308dd68331de983a39a5328d2bc8.jpg"
   artist2.save
 #seeding artists
-  artist3 = Artist.create!(first_name: "Chaïbia", last_name: "Tallal", description: "Peintre marocaine, pionniraire du style brut", birth_date: Date.new(1929,10,4), death_date: Date.new(2004,11,12))
+  artist3 = Artist.create!(first_name: "Chaïbia", last_name: "Tallal", description: "Peintre marocaine, pionniraire du style brut", birth_date: Date.new(1929,10,4), death_date: Date.new(2004,11,12), user_id: user1.id)
   artist3.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536571879/Capture_d_e%CC%81cran_2018-09-10_a%CC%80_10.31.00.png"
   artist3.save
 #seeding artists
-  artist4 = Artist.create!(first_name: "Saad", last_name: "Ben Cheffaj", description: "Artiste multidisciplinaire qui allie peinture et poésie", birth_date: Date.new(1939,10,4), death_date: Date.new(2003,11,12))
+  artist4 = Artist.create!(first_name: "Saad", last_name: "Ben Cheffaj", description: "Artiste multidisciplinaire qui allie peinture et poésie", birth_date: Date.new(1939,10,4), death_date: Date.new(2003,11,12), user_id: user1.id)
   artist4.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536573238/Capture_d_e%CC%81cran_2018-09-10_a%CC%80_10.53.30.png"
   artist4.save
 #seeding artists
-  artist5 = Artist.create!(first_name: "Shepard", last_name: "Fairey", description: "Artiste américain, sérigraphe, muraliste et illustrateur", birth_date: Date.new(1970,10,4), death_date: Date.new(2003,11,12))
+  artist5 = Artist.create!(first_name: "Shepard", last_name: "Fairey", description: "Artiste américain, sérigraphe, muraliste et illustrateur", birth_date: Date.new(1970,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist5.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536573488/Shepard-Fairey-featured-555x312.jpg"
   artist5.save
 #seeding artists
-  artist6 = Artist.create!(first_name: "Richard", last_name: "Mirando", description: "Alias Seen, reconnu comme le parain du graffiti", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist6 = Artist.create!(first_name: "Richard", last_name: "Mirando", description: "Alias Seen, reconnu comme le parain du graffiti", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist6.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536574052/Seen_Kool.jpg"
   artist6.save
 #seeding artists
-  artist7 = Artist.create!(first_name: "Hussein", last_name: "Tallal", description: "Artiste autodidacte", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist7 = Artist.create!(first_name: "Hussein", last_name: "Tallal", description: "Artiste autodidacte", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist7.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536578326/Capture_d_e%CC%81cran_2018-09-10_a%CC%80_12.18.33.png"
   artist7.save
 #seeding artists
-  artist8 = Artist.create!(first_name: "Choukri", last_name: "Regragui", description: "Génie du réalisme pictural", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist8 = Artist.create!(first_name: "Choukri", last_name: "Regragui", description: "Génie du réalisme pictural", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist8.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536579111/Choukri-Regragui-.jpg"
   artist8.save
 #seeding artists
-  artist9 = Artist.create!(first_name: "Abderrahim", last_name: "Iqbi", description: "Artiste tragique", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist9 = Artist.create!(first_name: "Abderrahim", last_name: "Iqbi", description: "Artiste tragique", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist9.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536579806/abderrahim_iqbi_nb.jpg"
   artist9.save
 #seeding artists
-  artist10 = Artist.create!(first_name: "Nabil", last_name: "Boudarqa", description: "Artiste tragique", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist10 = Artist.create!(first_name: "Nabil", last_name: "Boudarqa", description: "Artiste tragique", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user2.id)
   artist10.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536580099/articlesprincipaleskf3a8337.jpg"
   artist10.save
 #seeding artists
-  artist11 = Artist.create!(first_name: "Youssef", last_name: "Douieb", description: "Artiste moderne", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12))
+  artist11 = Artist.create!(first_name: "Youssef", last_name: "Douieb", description: "Artiste moderne", birth_date: Date.new(1961,10,4), death_date: Date.new(2003,11,12), user_id: user1.id)
   artist11.remote_photo_url = "https://res.cloudinary.com/artket/image/upload/v1536582137/249862_portrait_image.jpg"
   artist11.save
 #seeding artwork
