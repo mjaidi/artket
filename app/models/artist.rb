38c39327750f9e60dab9ec562cfbea1aa8f2b5e2
Yassmina Hannaoui <yas_h27@hotmail.fr>
2018-09-11 10:55:53 +0100

    exhib page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,10 +4,9 @@ class Artist < ApplicationRecord
   belongs_to :user
 
   mount_uploader :photo, PhotoUploader
-  
+
   validates :first_name, presence: :true
   validates_uniqueness_of :first_name, scope: :last_name
-  add_index :artists, [ :first_name, :last_name ], unique: true
 
   algoliasearch do
     # all attributes will be sent
